3e34fd0eb91bff4830e3175c318b877434d65acf
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2025-01-23 15:05:17 +0530

    feat: implement search with debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../store/appSlice";
 import { Link } from "react-router-dom";
+import { SUGGESTIONS_API } from "../utils/constants";
 
 const Head = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(SUGGESTIONS_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -28,11 +47,32 @@ const Head = () => {
         <input
           className="w-1/2 rounded-l-full border border-gray-400 p-2"
           type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
         />
         <button className="rounded-r-full border border-gray-400 px-3 py-2 bg-gray-100">
           🔍
         </button>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[26.5rem] shadow-lg rounded-lg border">
+            <ul>
+              {suggestions.map((s, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="py-2 px-3 shadow-sm hover:bg-gray-100"
+                  >
+                    🔍 {s}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
+
       <div className="col-span-1">
         <img
           className="h-8"
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -13,15 +13,15 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEO_API);
     const json = await data.json();
-    console.log(json);
+    // console.log(json);
     setVideos(json.items);
   };
   return (
     <div className="flex flex-wrap">
       {videos.map((video) => (
-        <Link to={"/watch?v=" + video.id}>
+        <Link to={"/watch?v=" + video.id} key={video.id}>
           {" "}
-          <VideoCard key={video.id} info={video} />
+          <VideoCard info={video} />
         </Link>
       ))}
     </div>
